c1c7feaff0ccbc98102f2efa1e55bf4b1f6123e3
POTTI-MOKSHAGNA <150547653+POTTI-MOKSHAGNA@users.noreply.github.com>
2023-12-20 20:35:39 +0530

    Bootcamp

---
+++ boot.py
@@ -0,0 +1,60 @@
+CSK = ["Ajay Mandal", "Ajinkya Rahane" , "Deepak Chahar" , "Devon Conway" , "Maheesh Theekshana" , "Matheesha Pathirana" , "Mitchell Santner" , "Moeen Ali" , "MS Dhoni",
+        "Mukesh Choudhary" , "Nishant Sindhu" , "Prashant Solanki" , "Rajvardhan Hangargekar" , "Ravindra Jadeja" , "Ruturaj Gaikwad" , "Shaik Rasheed" , "Shivam Dube",
+        "Simarjeet Singh" , "Tushar Deshpande" , "Rachin Ravindra" , "Shardul Thakur" , "Daryl Mitchell" , "Sameer Rizvi" , "Mustafizur Rahman" , "Avanish Rao Aravelly"]
+DC = ["Abhishek Porel" , "Anrich Nortje" , "Axar Patel" , "David Warner" , "Ishant Sharma" , "Kuldeep Yadav" , "Lalit Yadav" , "Lungisani Ngidi" , "Mitchell Marsh" , 
+      "Mukesh Kumar" , "Pravin Dubey" , "Prithvi Shaw" , "Rishabh Pant" , "Syed Khaleel Ahmed" , "Vicky Ostwal" , "Yash Dhull" , "Harry Brook" , "Tristan Stubbs" , 
+      "Ricky Bhui" , "Kumar Kushagra" , "Rasikh Dar" , "Jhye Richardson" , "Sumit Kumar" , "Shai Hope" , "Swastik Chhikara"]
+GT = ["Abhishek Porel" , "Anrich Nortje" , "Axar Patel" , "David Warner" , "Ishant Sharma" , "Kuldeep Yadav" , "Lalit Yadav" , "Lungisani Ngidi" , "Mitchell Marsh" ,
+      "Mukesh Kumar" , "Pravin Dubey" , "Prithvi Shaw" , "Rishabh Pant" , "Syed Khaleel Ahmed" , "Vicky Ostwal" , "Yash Dhull" , "Harry Brook" , "Tristan Stubbs" , 
+      "Ricky Bhui" , "Kumar Kushagra" , "Rasikh Dar" , "Jhye Richardson" , "Sumit Kumar" , "Shai Hope" , "Swastik Chhikara"]
+KKR = ["Nitish Rana" , "Rinku Singh" , "Rahmanullah Gurbaz" , "Shreyas Iyer" , "Jason Roy" , "Sunil Narine" , "Suyash Sharma" , "Anukul Roy" , "Andre Russell" , "Venkatesh Iyer"
+       "Harshit Rana" , "Vaibhav Arora" , "Varun Chakaravarthy" , "KS Bharat" , "Chetan Sakariya" , "Mitchell Starc" , "Angkrish Raghuvanshi" , "Ramandeep Singh" , "Sherfane Rutherford",
+       "Manish Pandey" , "Mujeeb Ur Rehman" , "Gus Atkinson" , "Sakib Hussain"]
+LSG = ["KL Rahul" , "Quinton de Kock" , "Nicholas Pooran" , "Ayush Badoni" , "Kyle Mayers" , "Marcus Stoinis" , "Deepak Hooda" , "Ravi Bishnoi" , "Naveen ul Haq" , "Krunal Pandya" ,
+       "Yudhvir Singh" , "Prerak Mankad" , "Yash Thakur" , "Amit Mishra" , "Mark Wood" , "Mayank Yadav" , "Mohsin Khan" , "K Gowtham" , "Devdutt Padikkal" , "Shivam Mavi" , "Arshin Kulkarni"
+       "M Siddharth" , "Ashton Turner" , "David Willey" , "Mohammed Arshad Khan"]
+MI = ["Rohit Sharma" , "Dewald Brevis" , "Suryakumar Yadav" , "Ishan Kishan" , "Tilak Verma" , "Tim David" , "Vishnu Vinod" , "Arjun Tendulkar" , "Shams Mulani" , "Nehal Wadhera"
+      "Jasprit Bumrah" , "Kumar Kartikeya" , "Piyush Chawla" , "Akash Madhwal" , "Jason Behrendorff" , "Hardik Pandya" , "Romario Shepherd" , "Gerald Coetzee" , "Dilshan Madushanka"
+      "Shreyas Gopal" , "Nuwan Thushara" , "Naman Dhir" , "Anshul Kamboj" , "Mohammad Nabi" , "Shivalik Sharma"]
+PBKS = ["Shikhar Dhawan" , "Liam Livingstone" , "Jonny Bairstow" , "Jitesh Sharma" , "Shivam Singh" , "Rahul Chahar" , "Prabhsimran Singh" , "Arshdeep Singh" , "Harpreet Bhatia" , 
+        "Harpreet Brar" , "Atharva Taide" , "Vidwath Kaverappa" , "Rishi Dhawan" , "Kagiso Rabada" , "Sam Curran" , "Nathan Ellis" , "Sikandar Raza" , "Harshal Patel" , "Chris Woakes" , 
+        "Ashutosh Sharma" , "Vishwanath Pratap Singh" , "Shashank Singh" , "Tanay Thyagarajan" , "Prince Choudhary" , "Rilee Rossouw"]
+RCB = ["Faf du Plessis" , "Glenn Maxwell" , "Virat Kohli" , "Rajat Patidar" , "Anuj Rawat" , "Dinesh Karthik" , "Suyash Prabhudessai" , "Will Jacks" , "Mahipal Lomror" , "Karn Sharma" ,
+       "Manoj Bhandage" , "Vyshak Vijaykumar" , "Akash Deep" , " Mohammed Siraj" , "Reece Topley" , "Himanshu Sharma" , "Rajan Kumar" , "Mayank Dagar" , "Cameron Green" , "Alzarri Joseph" , 
+       "Yash Dayal" , "Tom Curran" , "Lockie Ferguson" , "Swapnil Singh" , "Saurav Chuahan"]
+RR = ["Sanju Samson" , "Jos Buttler" , "Shimron Hetmyer" , "Yashasvi Jaiswal" , "Dhruv Jurel" , "Riyan Parag" , "Donovan Ferreira" , "Kunal Rathore" , "Ravichandran Ashwin" , "Kuldeep Sen" , 
+      "Navdeep Saini" , "Prasidh Krishna" , "Sandeep Sharma" , "Trent Boult" , "Yuzvendra Chahal" , "Adam Zampa" , "Avesh Khan" , "Rovman Powell" , "Shubham Dubey" , "Tom Kohler Cadmore" , 
+      "Nandre Burger" , "Abid Mushtaq"]
+SRH = ["Abdul Samad" , "Aiden Markram" , "Rahul Tripathi" , "Glenn Phillips" , "Heinrich Klaasen" , "Mayank Agarwal" , "Anmolpreet Singh" , "Upendra Singh Yadav" , "Nitish Kumar Reddy" ,
+        "Abhishek Sharma" , "Marco Jansen" , "Washington Sundar" , "Sanvir Singh" , "Bhuvneshwar Kumar" , "T Natarajan" ,"Mayank Markande" , "Umran Malik" , "Fazalhaq Farooqi" , "Shahbaz Ahmed" ,
+        "Travis Head" , "Wanindu Hasaranga" , "Pat Cummins" , "Jaydev Unadkat" , "Akash Singh" , "Jhathavedh Subramanyan"]
+a = input("Hi, Enter your name? ")
+print("Welcome,",a)
+print("I will tell the which player belongs to the IPL franchise team.")
+work = True
+while work:
+    b = input("Enter the player name: ")
+    if "exit" in b or "quit" in b :
+        work = False
+    elif b in CSK:
+        print(f"The player {b} belongs to the CSK.")
+    elif b in DC:
+        print(f"The player {b} belongs to the DC.")
+    elif b in GT:
+        print(f"The player {b} belongs to the GT.")
+    elif b in KKR:
+        print(f"The player {b} belongs to the KKR.")
+    elif b in LSG:
+        print(f"The player {b} belongs to the LSG.")
+    elif b in MI:
+        print(f"The player {b} belongs to the MI.")
+    elif b in PBKS:
+        print(f"The player {b} belongs to the PBKS.")
+    elif b in RCB:
+        print(f"The player {b} belongs to the RCB.")
+    elif b in RR:
+        print(f"The player {b} belongs to the RR.")
+    elif b in SRH:
+        print(f"The player {b} belongs to the SRH.")
+    else:
+        print("Please enter the name correctly")
